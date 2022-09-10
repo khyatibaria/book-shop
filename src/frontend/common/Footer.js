0c82a645b96689c50157599b566a6304a0b438c5
Saurabh Maurya <saurabhkumar04966@gmail.com>
2022-09-10 12:44:03 +0530

    feat footer

--- src/frontend/common/Footer.js
+++ src/frontend/common/Footer.js
@@ -1,45 +1,23 @@
 import "./Footer.css";
 import { images } from "../assets/index";
 const Footer = () => {
-  const { Twitter, Github, LinkedIn } = images;
+  const { Twitter, Github, LinkedIn, Heart } = images;
   return (
     <div className="footer-container">
-      <div className="footer-column">
-        <ul className="footer-links">
-          <li>Shipping</li>
-          <li>Payment</li>
-          <li>Return Policy</li>
-        </ul>
+      <div className="footer-head">
+        Made with <img src={Heart} className="footer-img" />
+        by Khyati Baria
       </div>
-      <div className="footer-column">
-        <ul className="footer-links">
-          <li>Books</li>
-          <li>Bestsellers</li>
-          <li>New Arrivals</li>
-        </ul>
-      </div>
-      <div className="footer-column">
-        <ul className="footer-links">
-          <li>khyatibaria55@gmail.com</li>
-          <li>link of website</li>
-          <div className="social-links">
-            <li>
-              <a href="https://github.com/khyatibaria/book-store">
-                <img src={Github} alt="github" />
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/khyatibaria">
-                <img src={Twitter} alt="github" />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/khyatibaria/">
-                <img src={LinkedIn} alt="github" />
-              </a>
-            </li>
-          </div>
-        </ul>
+      <div className="social-links">
+        <a href="https://github.com/khyatibaria/book-shop" target="_blank">
+          <img src={Github} alt="github" />
+        </a>
+        <a href="https://twitter.com/khyatibaria" target="_blank">
+          <img src={Twitter} alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/khyatibaria/" target="_blank">
+          <img src={LinkedIn} alt="github" />
+        </a>
       </div>
     </div>
   );
